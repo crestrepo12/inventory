5a7bcccc1052a389de0ba223d99f959e7622db03
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-11-03 23:11:21 -0400

    Added/get inventory value Modified/display inventory Modified/ID assigning

--- inventory.py
+++ inventory.py
@@ -10,21 +10,13 @@ class Item:
         total = self.price_per_unit * self.quantity
         return total
 
-    # print properties one by one here
-    def display_item_details(self):
-      item_details: {
-        "id": self.id,
-        "product name": self.product_name,
-        "price per unit": self.price_per_unit,
-        "quantity": self.quantity,
-        "product total": self.total_price()
-      }
-
-      self.id += 1
-
+    # Return all items
+    def get_items(self):
+        return (self.id, self.product_name,
+               self.price_per_unit, self.quantity,
+               self.total_price())
 
 # ---------------------------------------
-
 
 class Inventory(Item):
   def __init__(self):
@@ -33,32 +25,35 @@ class Inventory(Item):
 
   # adding each item to the inventory
   def add_to_inventory(self, product_name, price_per_unit, quantity):
-    Item(id, product_name, price_per_unit, quantity)
-
-    # have a loop to display all the items
-    
+    new_item = Item(product_name, price_per_unit, quantity, self.id)
 
     # add each item to the array of inventory_store
-    self.inventory_store.append(item_details)
+    self.inventory_store.append(new_item)
     self.id += 1
+
+  # Return the total inventory value
+  def get_inventory_value(self):
+    return sum([item.total_price() for item in self.inventory_store]) 
   
   # display the inventory store of all items that exist
   def display_inventory(self):
-    print(self.inventory_store)
+    for products in self.inventory_store:
+      id, product, price, quantity, total = products.get_items()
+      print("ID: {} | Product: {} | Price: {} | Quantity: {} | Total {}".format(
+        id, product, price, quantity, total
+      ))
 
+# # ----------------------------------------------
+# # Inputs
 
+# inventory = Inventory()
+# inventory.add_to_inventory("starbursts", 2, 5)
+# inventory.add_to_inventory("whoopers", 4, 6)
+# inventory.add_to_inventory("donuts", 2, 3)
+# inventory.add_to_inventory("adidas socks", 9, 13)
+# inventory.add_to_inventory("adidas kids shoes", 120, 11)
 
-# ----------------------------------------------
-# Inputs
+# # displaying inventory
 
-inventory = Inventory()
-inventory.add_to_inventory("starbursts", 2, 5)
-inventory.add_to_inventory("whoopers", 4, 6)
-inventory.add_to_inventory("donuts", 2, 3)
-inventory.add_to_inventory("adidas socks", 9, 13)
-inventory.add_to_inventory("adidas kids shoes", 120, 11)
-
-# displaying inventory
-
-inventory.display_inventory()
-
+# inventory.display_inventory()
+# print(inventory.get_inventory_value())
